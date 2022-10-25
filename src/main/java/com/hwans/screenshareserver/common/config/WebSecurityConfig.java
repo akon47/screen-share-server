7a35acf514503eea94680688e8d92e6b1a8a8397
Kim, Hwan <akon47@naver.com>
2022-10-25 21:14:06 +0900

    Update WebSecurityConfig.java

--- src/main/java/com/hwans/screenshareserver/common/config/WebSecurityConfig.java
+++ src/main/java/com/hwans/screenshareserver/common/config/WebSecurityConfig.java
@@ -29,7 +29,7 @@ public class WebSecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .httpBasic().disable()
-                .csrf().disable().cors()
+                .csrf().disable()
                 .cors().configurationSource(request -> corsConfiguration())
             .and()
                 .sessionManagement()
