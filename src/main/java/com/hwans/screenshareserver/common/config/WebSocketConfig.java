fa5d93a3ccd0ff2f6179011e41eb6dfb67351a69
Kim, Hwan <akon47@naver.com>
2022-10-30 17:57:31 +0900

    prod 환경에서 allowedOrigins이 따로 설정되도록 수정합니다.

--- src/main/java/com/hwans/screenshareserver/common/config/WebSocketConfig.java
+++ src/main/java/com/hwans/screenshareserver/common/config/WebSocketConfig.java
@@ -3,6 +3,7 @@ package com.hwans.screenshareserver.common.config;
 import com.hwans.screenshareserver.service.sharing.SharingService;
 import com.hwans.screenshareserver.service.sharing.SharingWebSocketHandler;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
@@ -14,10 +15,13 @@ import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
 public class WebSocketConfig implements WebSocketConfigurer {
     private final SharingWebSocketHandler sharingWebSocketHandler;
 
+    @Value("${allowedOrigins}")
+    private String[] allowedOrigins;
+
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         registry.addHandler(sharingWebSocketHandler, "/ws")
-                .setAllowedOriginPatterns("*")
+                .setAllowedOrigins(allowedOrigins)
                 .withSockJS();
     }
 }
